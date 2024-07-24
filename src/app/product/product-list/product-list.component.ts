87844c92b409998866d2f1768a9ccd7c013c55e4
Dan Morriss <danielmorriss1@gmail.com>
2024-07-24 11:21:33 +0200

    Add search to porducts

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/cart/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-list',
@@ -10,12 +12,37 @@ import { Product } from 'src/app/models/product';
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  filteredProducts: Product[] = [];
 
-  constructor(private productService: ProductService){}
+  constructor(private productService: ProductService,
+    private cartService: CartService,
+    private snackBar: MatSnackBar){}
   ngOnInit(): void {
     this.productService.getProducts().subscribe(data => {
       this.products = data;
+      this.filteredProducts = data;
     });
+  }
+
+  addToCart(product: Product): void {
+    this.cartService.addToCart(product).subscribe({
+      next: () => {
+        this.snackBar.open("Product added to cart!", "", {
+          duration: 2000,
+          horizontalPosition: 'center',
+          verticalPosition: 'top'
+        })
+      }
+    });
+  }
+
+  applyFilter(event: Event): void {
+    let searchTerm = (event.target as HTMLInputElement).value;
+    searchTerm = searchTerm.toLowerCase();
+
+    this.filteredProducts = this.products.filter(
+      product => product.name.toLowerCase().includes(searchTerm)
+    )
   }
 
 }
